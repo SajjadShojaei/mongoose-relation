a44a1aa39ee5bde829a845e9c32c9e8ac9f69db1
Sajjad Shojaei Moghaddam <sajjadshojaeimoghaddam@gmail.com>
2022-05-31 11:51:34 +0430

    update DTO

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Address } from "src/schemas/address.schema";
 
 export class UserDto {
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     address: string;
 }

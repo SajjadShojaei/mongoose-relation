5610a528807a84c7694d5f52a02bce985dbe4506
Sajjad Shojaei Moghaddam <sajjadshojaeimoghaddam@gmail.com>
2022-05-31 11:39:27 +0430

    config mongo and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AddressModule } from './address/address.module';
-
+require('dotenv').config();
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, AddressModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODBURL, { dbName: 'nest' }),
+            ConfigModule.forRoot({ envFilePath: '../../.env' }),
+            UserModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
